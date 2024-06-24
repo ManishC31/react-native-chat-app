0ece1685cbfc4b41f7d43dc9c34183666ecbedfd
ManishC31 <manishchavan80@gmail.com>
2024-06-24 23:01:28 +0530

    first commit

---
+++ app/(tabs)/third.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const third = () => {
+  return (
+    <View>
+      <Text>third</Text>
+    </View>
+  )
+}
+
+export default third
+
+const styles = StyleSheet.create({})
